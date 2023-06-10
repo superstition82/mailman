ac799ff9fb24c4268c684f88f5bde296f58b8975
superposition82 <chotnt741@gmail.com>
2023-06-10 23:27:14 +0900

    add padding main section

--- web/src/layouts/Root.tsx
+++ web/src/layouts/Root.tsx
@@ -6,7 +6,7 @@ function Root() {
     <div className="w-full min-h-full bg-zinc-100 ">
       <div className="w-full h-full max-w-6xl mx-auto flex flex-row justify-center items-start sm:px-4">
         <Header />
-        <main className="w-auto h-full max-w-full flex-grow shrink flex flex-col justify-start items-start">
+        <main className="w-auto h-full max-w-full flex-grow shrink flex flex-col justify-start items-start pt-6">
           <Outlet />
         </main>
       </div>
